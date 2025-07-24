a307ccb535da756f0eba02d894bb71fe8c81376f
Theuzz1552 <theuzz1552@gmail.com>
2025-07-24 23:15:04 +0000

    cartas finalizadas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,13 +5,13 @@ int main(){
 printf("Desafio Cartas Super Trunfo!\n");
 printf("Carta1\n");
 
-char estado[50];
-char codigo[50];
-char cidade[50];
-int população;
-float area;
-float PIB;
-int NumeroPT;
+char estado[50], estado2[50];
+char codigo[50], codigo2[50];
+char cidade[50], cidade2[50];
+int população, população2;
+float area, area2;
+float PIB, PIB2;
+int NumeroPT, NumeroPT2;
 
 printf("estado: ");
 scanf("%s", &estado);
@@ -33,6 +33,29 @@ scanf("%f", &PIB);
 
 printf("NumeroPT: ");
 scanf("%d", &NumeroPT);
+
+printf("Carta2\n");
+
+printf("estado: ");
+scanf("%s", &estado2);
+
+printf("codigo: ");
+scanf("%s", &codigo2);
+
+printf("cidade: ");
+scanf("%s", &cidade2);
+
+printf("população: ");
+scanf("%d", &população2);
+
+printf("area: ");
+scanf("%f", &area2);
+
+printf("PIB: ");
+scanf("%f", &PIB2);
+
+printf("NumeroPT: ");
+scanf("%d", &NumeroPT2 );
 
 
 }
