99bbefcd2e88d79849d2a31d468204a5a877a591
Theuzz1552 <theuzz1552@gmail.com>
2025-07-26 03:58:51 +0000

    nivel novato concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,60 +2,33 @@
 
 int main(){
 
+char estado = 'A', estado2 = 'B';
+char codigo[20] = "A01", codigo2[20] = "B02";
+char cidade[20] = "Sao Paulo", cidade2[20] = "Rio de Janeiro";
+int populacao = 12325000, populacao2 = 6748000;
+float area = 1521.11, area2 =  120.25;
+float PIB = 699.28, PIB2 = 300.50;
+int PT = 50, PT2 = 30;
+
 printf("\n--- Dados da Carta 1 ---\n");
 
-char estado[50], estado2[50];
-char codigo[50], codigo2[50];
-char cidade[50], cidade2[50];
-int populacao, populacao2;
-float area, area2;
-float PIB, PIB2;
-int NumeroPT, NumeroPT2;
-
-printf("estado: ");
-scanf("%s", estado);
-
-printf("codigo: ");
-scanf("%s", codigo);
-
-printf("cidade: ");
-scanf("%s", &cidade);
-
-printf("populacao: ");
-scanf("%d", &populacao);
-
-printf("area: ");
-scanf("%f", &area);
-
-printf("PIB: ");
-scanf("%f", &PIB);
-
-printf("NumeroPT: ");
-scanf("%d", &NumeroPT);
+printf("Estado: %c\n", estado);
+printf("Codigo: %s\n", codigo);
+printf("Cidade: %s\n", cidade);
+printf("População: %d\n", populacao);
+printf("Area: %.2f km²\n", area);
+printf("PIB: %.2f bilhoes de reais\n", PIB);
+printf("Numero de Pontos Turisticos: %d\n", PT);
 
 printf("\n--- Dados da Carta 2 ---\n");
 
-printf("estado: ");
-scanf("%s", &estado2);
-
-printf("codigo: ");
-scanf("%s", codigo2);
-
-printf("cidade: ");
-scanf("%s", cidade2);
-
-printf("populacao: ");
-scanf("%d", &populacao2);
-
-printf("area: ");
-scanf("%f", &area2);
-
-printf("PIB: ");
-scanf("%f", &PIB2);
-
-printf("NumeroPT: ");
-scanf("%d", &NumeroPT2 );
-
+printf("Estado: %c\n", estado2);
+printf("Codigo: %s\n", codigo2);
+printf("Cidade: %s\n", cidade2);
+printf("População: %d\n", populacao2);
+printf("Area: %.2f km²\n", area2);
+printf("PIB: %.2f bilhoes de reais\n", PIB2);
+printf("Numero de Pontos Turisticos: %d\n", PT2);
 
 return 0;
 
