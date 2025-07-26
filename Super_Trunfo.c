900bd205682de58c8b038778b104acd46eff5dfc
Theuzz1552 <theuzz1552@gmail.com>
2025-07-26 23:24:48 +0000

    Nivel intermediario concluido

---
+++ Super_Trunfo.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+int main(){
+
+    char estado[10], estado2[10];
+    char codigo[10], codigo2[10];
+    char cidade[20], cidade2[20];
+    int populacao, populacao2;
+    float area, area2;
+    float PIB, PIB2;
+    int PT, PT2;
+    float densidade, densidade2;
+    float percapita, percapita2;
+
+    printf("\n--- Dados da Carta 1 ---\n");
+
+    printf("Estado: ");
+    scanf("%s", estado);
+
+    printf("Codigo: ");
+    scanf("%s", codigo);
+
+    printf("Cidade: ");
+    scanf("%s", cidade);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area: ");
+    scanf("%f", &area);
+
+    printf("PIB: ");
+    scanf("%f", &PIB);
+
+    printf("Numero de Pontos Turisticos: ");
+    scanf("%d", &PT);
+
+    densidade = populacao / area;
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade);
+
+    PIB *= 1000000000; // Multiplicando PIB por 1 bilhão para ajustar para reais
+    percapita = PIB / populacao;
+    printf("PIB Per Capita: %.2f reais\n", percapita);
+
+    printf("\n--- Dados da Carta 2 ---\n");
+
+    printf("Estado: ");
+    scanf("%s", estado2);
+
+    printf("Codigo: ");
+    scanf("%s", codigo2);
+
+    printf("Cidade: ");
+    scanf("%s", cidade2);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao2);
+
+    printf("Area: ");
+    scanf("%f", &area2);
+
+    printf("PIB: ");
+    scanf("%f", &PIB2);
+
+    printf("Numero de Pontos Turisticos: ");
+    scanf("%d", &PT2);
+
+    densidade2 = populacao2 / area2;
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+
+    PIB2 *= 1000000000; // Multiplicando PIB por 1 bilhão para ajustar para reais
+    percapita2 = PIB2 / populacao2;
+    printf("PIB Per Capita: %.2f reais\n", percapita2);
+
+    return 0;
+
+} 
